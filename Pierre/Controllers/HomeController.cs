4fb661cca7edbc68c2f7c51d4109393caa61fede
ambi hidalgo <ambio.pk@gmail.com>
2023-04-08 14:18:44 -0700

    Add Initial Migration

---
+++ Pierre/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Pierre.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace Pierre.Controllers
+{
+  public class HomeController: Controller
+  {
+
+  }
+}
