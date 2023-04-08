989187c9bc5b1d12609c92a4da6756f4fafdb306
ambi hidalgo <ambio.pk@gmail.com>
2023-04-08 14:07:37 -0700

    Add project infrastructure

---
+++ Pierre/Models/PierreContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace Pierre.Models
+{
+  public class PierreContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+
+    public DbSet<TreatFlavor> TreatFlavors { get; set; }
+
+     public PierreContext(DbContextOptions options) : base(options) { }
+    
+
+    
+  }
+}
